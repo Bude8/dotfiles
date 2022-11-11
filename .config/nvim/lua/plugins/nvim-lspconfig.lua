512773f8d55c29530b7e72cbf72c66136a81fc73
Henry Lee <henry.lee@bidfx.com>
2022-11-11 23:52:07 +0000

    nvim: lsp add gopls

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -140,10 +140,12 @@ local root_dir = function()
   return vim.fn.getcwd()
 end
 
+require'lspconfig'.gopls.setup{}
+
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches.
 -- Add your language server below:
-local servers = { 'bashls', 'pyright', 'clangd', 'html', 'cssls', 'tsserver', 'yamlls' }
+local servers = { 'bashls', 'pyright', 'clangd', 'html', 'cssls', 'tsserver', 'yamlls', 'gopls' }
 
 -- Call setup
 for _, lsp in ipairs(servers) do
