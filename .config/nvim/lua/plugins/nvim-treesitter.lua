7473be120e2301a0aecf9e1da093257917c408fc
Henry Lee <henry.lee@bidfx.com>
2023-01-27 11:33:54 +0000

    nvim: remove terraform from treesitter

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -16,7 +16,7 @@ nvim_treesitter.setup {
   -- A list of parser names, or "all"
   ensure_installed = {
     'bash', 'c', 'cpp', 'css', 'help', 'hcl', 'html', 'javascript', 'json', 'lua', 'python',
-    'typescript', 'yaml', 'vim', 'go', 'rust', 'terraform'
+    'typescript', 'yaml', 'vim', 'go', 'rust'
   },
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
